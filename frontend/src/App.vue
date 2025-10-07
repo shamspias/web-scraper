65fa02849804d8c8c4e9712de6bdb6bf9160d423
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-10-07 14:09:45 +0300

    feat: app view

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="min-h-screen bg-gradient-to-br from-gray-50 via-blue-50 to-gray-50">
+    <router-view/>
+  </div>
+</template>
+
+<script setup>
+import {onMounted} from 'vue'
+import {useScraperStore} from './stores/scraper'
+
+const scraperStore = useScraperStore()
+
+onMounted(() => {
+  scraperStore.checkApiHealth()
+})
+</script>
