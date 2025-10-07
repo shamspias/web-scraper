6a11fe38a00a27e24f6ed8da79a8522cb9b6da9a
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-10-07 14:11:31 +0300

    feat: landing spiner

---
+++ frontend/src/components/LoadingSpinner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center justify-center p-8">
+    <div class="relative">
+      <div class="w-16 h-16 border-4 border-primary-200 border-t-primary-600 rounded-full animate-spin"></div>
+      <div class="absolute inset-0 flex items-center justify-center">
+        <svg class="w-8 h-8 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
